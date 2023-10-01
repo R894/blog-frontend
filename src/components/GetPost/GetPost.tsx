aa5299a9a4a9cc940463598f9d6a16fa1cd068a0
R894 <kamil.khalaileh@gmail.com>
2023-10-01 19:09:57 +0300

    add individual post page

---
+++ src/components/GetPost/GetPost.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import api from "../../api/axios";
+import GetPostCSS from './getpost.module.css';
+import { useParams } from "react-router-dom";
+interface DataProps {
+    _id: string;
+    title: string;
+    pubDate: string;
+    content: string;
+}
+
+const GetPost = () => {
+    const { id } = useParams();
+    const [data, setData] = useState<DataProps>({_id:'', title:'', pubDate:'', content:''});
+
+    useEffect(()=> {
+        api.get(`/posts/${id}`).then((res) => {
+            setData(res.data);
+        })
+    })
+
+    return(
+        <div className={GetPostCSS.card}>
+              <h3>{data.title}</h3>
+              <p>{new Date(data.pubDate).toLocaleString()}</p>
+              <p>{data.content}</p>
+        </div>
+    );
+};
+export default GetPost;
---
+++ src/pages/Post.tsx
@@ -0,0 +1,14 @@
+import Layout from '../layouts/Layout';
+import GetPost from '../components/GetPost/GetPost';
+
+const GetPostsPage = () => {
+    return(
+        <>
+            <Layout>
+                <GetPost/>
+            </Layout>
+        </>
+    );
+}
+
+export default GetPostsPage;
