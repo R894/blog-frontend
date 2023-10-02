c5f791bb7d785b3ded8acab5641499692ae9390d
R894 <kamil.khalaileh@gmail.com>
2023-10-02 20:22:23 +0300

    add author name to individual posts

--- src/components/GetPost/GetPost.tsx
+++ src/components/GetPost/GetPost.tsx
@@ -1,30 +1,41 @@
 import { useEffect, useState } from "react";
 import api from "../../api/axios";
-import GetPostCSS from './getpost.module.css';
+import GetPostCSS from "./getpost.module.css";
 import { useParams } from "react-router-dom";
 interface DataProps {
-    _id: string;
-    title: string;
-    pubDate: string;
-    content: string;
+  _id: string;
+  title: string;
+  pubDate: string;
+  content: string;
+  author: {
+    username: string;
+  };
 }
 
 const GetPost = () => {
-    const { id } = useParams();
-    const [data, setData] = useState<DataProps>({_id:'', title:'', pubDate:'', content:''});
+  const { id } = useParams();
+  const [data, setData] = useState<DataProps>({
+    _id: "",
+    title: "",
+    pubDate: "",
+    content: "",
+    author: { username: "" },
+  });
 
-    useEffect(()=> {
-        api.get(`/posts/${id}`).then((res) => {
-            setData(res.data);
-        })
-    }, [id])
+  useEffect(() => {
+    api.get(`/posts/${id}`).then((res) => {
+      console.log(res.data);
+      setData(res.data);
+    });
+  }, [id]);
 
-    return(
-        <div className={GetPostCSS.card}>
-              <h3>{data.title}</h3>
-              <p>{new Date(data.pubDate).toLocaleString()}</p>
-              <p>{data.content}</p>
-        </div>
-    );
+  return (
+    <div className={GetPostCSS.card}>
+      <h3>{data.title}</h3>
+      <p>{new Date(data.pubDate).toLocaleString()}</p>
+      <p>{data.author.username}</p>
+      <p>{data.content}</p>
+    </div>
+  );
 };
 export default GetPost;
