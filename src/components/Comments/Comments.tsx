ebcbaaf3df0ce389f124d446de49943691e72fb1
R894 <kamil.khalaileh@gmail.com>
2023-10-03 22:44:01 +0300

    add comments feature

---
+++ src/components/Comments/Comments.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import api from "../../api/axios";
+import { useParams } from "react-router-dom";
+import CommentsCSS from "./comments.module.css";
+
+const Comments = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const { id } = useParams();
+
+  interface commentInterface {
+    user: string;
+    content: string;
+  }
+
+  useEffect(() => {
+    api.get(`/posts/${id}/comments`).then((res) => {
+      setIsLoading(false);
+      setData(res.data);
+      console.log(res.data);
+    });
+  }, [id]);
+
+  const mapComments = (commentData: commentInterface[]) => {
+    if (commentData.length === 0) {
+      return <p>No comments</p>;
+    }
+    return commentData.map((comment, index: number) => (
+      <div key={index} className={CommentsCSS.comment}>
+        <p id="user">{comment.user}</p>
+        <p id="comment">{comment.content}</p>
+      </div>
+    ));
+  };
+
+  return <>{isLoading ? <p>Loading...</p> : 
+  <div className={CommentsCSS.div}>
+    {mapComments(data)}
+  </div>
+  }</>;
+};
+export default Comments;
--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,11 +1,13 @@
 import Layout from '../layouts/Layout';
 import GetPost from '../components/GetPost/GetPost';
+import Comments from '../components/Comments/Comments';
 
 const GetPostsPage = () => {
     return(
         <>
             <Layout>
                 <GetPost/>
+                <Comments/>
             </Layout>
         </>
     );
