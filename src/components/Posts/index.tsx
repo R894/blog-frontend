ddd46192a7936bd22cad647f7b45507b4289128b
R894 <kamil.khalaileh@gmail.com>
2023-11-16 23:08:16 +0200

    more responsiveness changes

--- src/components/Posts/index.tsx
+++ src/components/Posts/index.tsx
@@ -44,8 +44,8 @@ const Posts = () => {
           <p className="text-2xl mb-8 font-bold">All blog posts</p>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-7">
             {data.map((item) => (
-              <div key={item.id} className="w-96 h-auto">
-                <div className="w-96 h-60 bg-slate-500 mb-8"></div>
+              <div key={item.id} className="w-80 xl:w-96 h-auto">
+                <div className="w-80 xl:w-96 h-60 bg-slate-500 mb-8"></div>
                 <p className="text-purple-800 text-sm font-semibold">{new Date(item.createdAt).toLocaleString()} • {item.userId}</p>
                 <h3 className="text-2xl font-bold tracking-tight text-gray-900 mb-3 my-3"><Link to={`/posts/${item.id}`}>{item.title}</Link></h3>
                 <p className=" text-gray-500">{reduceString(item.content, 150)}</p>
