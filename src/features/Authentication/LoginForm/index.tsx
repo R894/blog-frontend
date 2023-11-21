4b89a09ebaa584bf7f8b4fee83b8e93dc9f8545c
R894 <kamil.khalaileh@gmail.com>
2023-11-21 20:58:36 +0200

    modify login form css

--- src/features/Authentication/LoginForm/index.tsx
+++ src/features/Authentication/LoginForm/index.tsx
@@ -1,7 +1,7 @@
 import { useState, ChangeEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import useAuth from "../../../hooks/useAuth";
-import api from "../../../api/axios";
+import apiService from "../../../api/axios";
 
 const LoginForm = () => {
   const { setAuth } = useAuth();
@@ -32,8 +32,7 @@ const LoginForm = () => {
     e.preventDefault();
     setIsLoading(true);
 
-    api
-      .post(`/login`, formData)
+    apiService.login(formData.username, formData.password)
       .then((res) => {
         localStorage.setItem("accessToken", res.data.message);
         setAuth(res.data);
@@ -48,15 +47,17 @@ const LoginForm = () => {
   };
 
   return (
-    <>
-      <div className="">
-        <h2>Login</h2>
-        <form onSubmit={handleSubmit}>
+    <div className="flex justify-center">
+      <div className="flex flex-col gap-3 text-center w-80">
+        <h1 className="text-3xl">Welcome back!</h1>
+        <p>Sign in to get the most out of Nuntium</p>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3 text-center">
           <div>
             <input
               type="text"
               id="username"
               name="username"
+              className="w-full h-14 p-3 rounded-lg"
               placeholder="Username"
               value={formData.username}
               onChange={handleChange}
@@ -68,6 +69,7 @@ const LoginForm = () => {
               type="password"
               id="password"
               name="password"
+              className="w-full h-14 p-3 rounded-lg"
               placeholder="Password"
               value={formData.password}
               onChange={handleChange}
@@ -75,13 +77,13 @@ const LoginForm = () => {
             />
           </div>
           <div>
-            <button type="submit">
+            <button type="submit" className="w-full h-14 border-2 rounded-lg border-black">
               {isLoading ? "Logging in..." : "Login"}
             </button>
           </div>
         </form>
       </div>
-    </>
+    </div>
   );
 };
 
