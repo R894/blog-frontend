15edbcdea2a23e89eeed838ae9b7dce0e60b5eeb
R894 <kamil.khalaileh@gmail.com>
2023-11-21 20:59:07 +0200

    add missing key attribute to posts map

--- src/features/Posts/index.tsx
+++ src/features/Posts/index.tsx
@@ -42,7 +42,7 @@ const Posts = () => {
           <div className="flex justify-center">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-7">
               {data.map((item) => (
-                <PostContainer {...item} />
+                <PostContainer {...item} key={item.id} />
               ))}
             </div>
           </div>
